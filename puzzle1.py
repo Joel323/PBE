81d143e6d2fdbccf8d6314fa54e0873ec7b6122d
Your Name <you@example.com>
2021-10-03 22:09:38 +0100

    curses

--- puzzle1.py
+++ puzzle1.py
@@ -1,11 +1,22 @@
+import curses
+
 class Rfid:
-	def read_uid(self):
-		uid = int(input())
-		uidHex = hex(uid)
-		return uidHex.upper()
+    def read_uid(self):
+        stdsrc = curses.initscr()
+        curses.noecho()
+        uidString = stdsrc.getstr(0,0)
+        uid = int(uidString)
+        uidHex = hex(uid)
+        curses.echo()
+        curses.endwin()
+        return uidHex.upper()
+         
+        
 
 if __name__ == "__main__":
-	rf = Rfid()
-	while(1):
-		uid = rf.read_uid()
-		print(uid)
+    rf = Rfid()
+    uid = rf.read_uid()
+    print(uid)
+
+        
+
